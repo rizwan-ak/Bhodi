8b60cc98a9f88a04d9b463ebde3ba70d9159fb87
Rizwan Ahmad <rizwan.ak@outlook.com>
2020-06-25 00:56:01 +0500

    Updates

---
+++ precache-manifest.2adff2e3792ce7fbb51da1c2b5efc63c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3aa87575f0a7411fe2122bccbee1b35",
+    "url": "/bhodi/index.html"
+  },
+  {
+    "revision": "bec97982a85997a1b5d1",
+    "url": "/bhodi/static/css/main.6d5964be.chunk.css"
+  },
+  {
+    "revision": "11fc238ae8ddae2f5d2e",
+    "url": "/bhodi/static/js/2.c97d8be1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bhodi/static/js/2.c97d8be1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bec97982a85997a1b5d1",
+    "url": "/bhodi/static/js/main.cefac829.chunk.js"
+  },
+  {
+    "revision": "df7c232a52f84513f967",
+    "url": "/bhodi/static/js/runtime-main.7d4d0796.js"
+  },
+  {
+    "revision": "6d1e2a04b0f2b59f19423fd1ebefd8b5",
+    "url": "/bhodi/static/media/bgImg.6d1e2a04.jpg"
+  },
+  {
+    "revision": "aebe360d72541da807adfb6c84749517",
+    "url": "/bhodi/static/media/dp.aebe360d.jpg"
+  }
+]);
